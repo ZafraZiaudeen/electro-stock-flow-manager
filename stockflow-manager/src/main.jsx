4110bc51fe96f1b52cb54c75a6f0ad43d7f1f1f3
Zafra Ziaudeen <zafraziaudeen@gmail.com>
2025-05-30 13:31:19 +0530

    designed login

--- stockflow-manager/src/main.jsx
+++ stockflow-manager/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import LoginPage from './pages/login.page';
+import RootLayout from './layouts/root.layout';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          
+          <Route path="/sign-in" element={<LoginPage />} />
+       
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  
+
+  </StrictMode>
 )
