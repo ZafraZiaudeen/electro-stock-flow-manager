f14662228b2ca6f8c3693d96ac5842ab7d65852c
Zafra Ziaudeen <zafraziaudeen@gmail.com>
2025-10-20 11:43:34 +0530

    edited the api be url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = "https://electro-stockflow-be.onrender.com";
 
 export const api = createApi({
   reducerPath: "api",
